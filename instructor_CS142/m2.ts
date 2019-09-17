8e5bd134c1b70943f83deabe372d66d0825d2f1d
Clark Ngo <clarkngo@gmail.com>
2019-09-17 05:16:48 -0700

    del file

--- instructor_CS142/m2.ts
+++
@@ -1,8 +0,0 @@
-
-
-
-// Multi Type Array
-
-// let values: (string | number)[] = ['Apple', 2, 'Orange', 3, 4, 'Banana'];
-// // or
-// let values: Array<string | number> = ['Apple', 2, 'Orange', 3, 4, 'Banana'];
